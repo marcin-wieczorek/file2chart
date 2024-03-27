e41a5b1bc39bfd21e963c785c0ea1917e284a13e
Marcin Wieczorek <mwieczorek@man.poznan.pl>
2024-03-27 10:22:23 +0100

    Optimize import

--- src/main/java/com/file2chart/service/utils/SecureRedirectService.java
+++ src/main/java/com/file2chart/service/utils/SecureRedirectService.java
@@ -5,7 +5,6 @@ import lombok.AllArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
